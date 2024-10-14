a3d9fc0f80343828798885f95740ff4c30be7b28
riyachiraniya <riyachiraniya5@gmail.com>
2024-10-14 19:21:32 +0530

    Services Page updated

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/navbar"
-import About from "./pages/about"
-import Services from "./pages/services"
-//import Contact from "./pages/contact"
-import Calendar from "./pages/calender"
-import Home from "./pages/home"
+import Navbar from "./components/navbar";
+import About from "./pages/about";
+import Services from "./pages/services"; // Ensure this import is correct
+import Calendar from "./pages/calender";
+import Home from "./pages/home";
 
 function App() {
   return (
     <div className="App">
-      <div className ='NavBar'>
-      <Router>
-      <Navbar/>
-      <Routes>
-          <Route path = '/home' element = {<Home/>} />
-          <Route path='/about' element={<About />} />
-          <Route path='/calendar' element={<Calendar />} />
-          <Route path='/services' element={<Services />} />
-          {/* <Route path='/contact' element={<Contact />} /> */}
-        </Routes>
-      </Router>
+      <div className='NavBar'>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path='/home' element={<Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/calendar' element={<Calendar />} />
+            <Route path='/services' element={<Services />} /> {/* Ensure this route is correct */}
+            {/* <Route path='/contact' element={<Contact />} /> */}
+          </Routes>
+        </Router>
       </div>
     </div>
   );

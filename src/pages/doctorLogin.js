9e25f35af32962a1f67c19c5f91ab74c1c054918
JhaAkansha <akanshajha47@gmail.com>
2024-10-18 01:06:36 +0530

    login and sign up page for doctor

--- src/pages/doctorLogin.js
+++ src/pages/doctorLogin.js
@@ -1,7 +1,10 @@
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 import './doctorLogin.css';
+import { CSSTransition } from 'react-transition-group';
 
 function DoctorLogin() {
+  const [inProp, setInProp] = useState(true);
   const navigate = useNavigate();
 
   const handleSubmit = (event) => {
@@ -26,28 +29,39 @@ function DoctorLogin() {
     }
   };
 
+  const handleSignUpClick = () => {
+    setInProp(false);
+    setTimeout(() => {
+      navigate('/doctorSignUp');
+    }, 200);
+  };
+
   return (
+    <CSSTransition in={inProp} timeout={200} classNames="fade" appear>
     <div className='doctor-login'>
-    <div className="login-container">
-      <h2>Sign In</h2>
-      <form id="doctorLoginForm" onSubmit={handleSubmit}>
-        <div className='alt-login-opts'>
-        <svg className='google-icon' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 488 512"><path fill="#16423c" d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"/></svg>
-        <div className='text'> sign in using google</div>
-        </div>
-        <div className='or'>or</div>
-        <input placeholder = 'Email' className = 'email' type="email" id="doctorEmail" name="doctorEmail" required />
-        <input placeholder = 'Password' className='password' type="password" id="doctorPassword" name="doctorPassword" required />
-        <div className='forgot_password'>Forgot Password?</div>
-        <button className="login">Login</button>
-      </form>
+      <div className="login-container">
+        <h2>Sign In</h2>
+        <form id="doctorLoginForm" onSubmit={handleSubmit}>
+          <div className='alt-login-opts'>
+            <svg className='google-icon' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 488 512">
+              <path fill="#16423c" d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"/>
+            </svg>
+            <div className='text'>Sign in using Google</div>
+          </div>
+          <div className='or'>or</div>
+          <input placeholder='Email' className='email' type="email" id="doctorEmail" name="doctorEmail" required />
+          <input placeholder='Password' className='password' type="password" id="doctorPassword" name="doctorPassword" required />
+          <div className='forgot_password'>Forgot Password?</div>
+          <button className="login">Login</button>
+        </form>
+      </div>
+      <div className='content'>
+        <h2 className='hello'>Hello, Friend!</h2>
+        <div className='para'>Register with your personal details to use all site features.</div>
+        <button className='sign-up' onClick={handleSignUpClick}>Sign Up</button>
+      </div>
     </div>
-    <div className='content'>
-      <h2 className='hello'> Hello, Friend!</h2>
-      <div className='para'>Register with your personal details to use all site features.</div>
-      <button className='sign-up'> Sign Up</button>
-    </div>
-    </div>
+    </CSSTransition>
   );
 }
 

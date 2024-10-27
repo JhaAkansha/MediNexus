e401e051ea20d7cd025c5120debe1cd165f65747
JhaAkansha <akanshajha47@gmail.com>
2024-10-27 21:22:33 +0530

    review form

--- src/pages/addReview.js
+++ src/pages/addReview.js
@@ -4,34 +4,32 @@ import './addReview.css';
 function AddReview() {
 
   const [formData, setFormData] = useState ({
-    dcotor: '',
+    name: '',
+    doctor: '',
     experience:'',
   });
 
   const handleChange = (e) => {
-    setFormData(...formData);
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-
-    // Get form data
-    const reviewerName = document.getElementById('reviewerName').value;
-    const doctorName = document.getElementById('doctorName').value;
-
-    // Validate form inputs (add your validation logic here)
-    if (!reviewerName|| !doctorName) {
-      alert('Please fill in all fields.');
-      return;
-    }
+    // Handle form submission, such as sending data to the backend or displaying an alert
+    alert('Thank you for your review!');
+    console.log(formData);
   };
 
   return (
       <div className="review-container">
+        <div className='review-form'>
         <h2 className='review-heading'>Let us know about your experience!</h2>
         <form className="reviewForm" onSubmit={handleSubmit}>
+        <label>What do you want your name to appear as?</label>
+        <input type="text" name="name" value={formData.name} onChange={handleChange} required />
           <label>Who treated you?</label>
-          <select name="doctor" value={formData.doctor} onChange={handleChange} required>
+          <select name="doctor" className= 'doctor-select'value={formData.doctor} onChange={handleChange} required>
             <option value="">Select Doctor</option>
             <option value="Dr. John Doe">Dr. John Doe</option>
             <option value="Dr. Jane Smith">Dr. Jane Smith</option>
@@ -39,7 +37,9 @@ function AddReview() {
           </select>
           <label>How satisfied were you with the service?</label>
           <label>How was your experience with the doctor?</label>
+          <button className= 'submit-button' type="submit">Submit</button>
         </form>
+        </div>
       </div>
   );
 }

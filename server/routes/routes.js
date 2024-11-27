3e5455dcf86669850c5b7a823bb197e2f8d2dca5
JhaAkansha <akanshajha47@gmail.com>
2024-11-27 12:49:03 +0530

    sending user data to users database

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,12 +1,23 @@
 const express = require('express');
+const Model = require('../models/model');
 
 const router = express.Router()
 
 module.exports = router;
 
 //Post Method
-router.post('/post', (req, res) => {
-    res.send('Post API')
+router.post('/post', async (req, res) => {
+    const data = new Model({
+        name: req.body.name,
+        password: req.body.password
+    })
+    try{
+        const dataToSave = await data.save();
+        res.status(200).json(dataToSave)
+    }
+    catch(error){
+        res.status(400).json({message:error.message})
+    }
 })
 
 //Get all Method

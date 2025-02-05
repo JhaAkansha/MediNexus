17b62733a07872f88284ee7e5077907bc570dd99
JhaAkansha <akanshajha47@gmail.com>
2025-02-05 13:44:13 +0530

    Code Cleaning

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,6 @@
 // about.js
 import React from 'react';
-import './about.css'; // Ensure this points to your CSS file
+import './about.css';
 
 function About() {
   return (
@@ -8,7 +8,7 @@ function About() {
       <div className="about-content">
         <h1 className="about-heading">About Us</h1>
       </div>
-      <div className="vertical-line"></div> {/* Vertical Line */}
+      <div className="vertical-line"></div>
       <div className="info">
         <div>
           Medinexus is more than just a digital solution; it's a transformative tool that revolutionizes the way appointments are managed in the healthcare industry. Our system prioritizes efficiency, security, and convenience, ensuring a seamless experience for all users. Join us in enhancing healthcare accessibility and efficiency for a better tomorrow.

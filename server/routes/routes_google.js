33354a2c058e587976703b2e2fe99e2567db123e
JhaAkansha <akanshajha47@gmail.com>
2025-03-05 18:36:20 +0530

    Code Cleaning

--- server/routes/routes_google.js
+++ server/routes/routes_google.js
@@ -3,9 +3,8 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const axios = require('axios'); // To make HTTP requests to Google API
+const axios = require('axios');
 
-// Regular login (email and password)
 router.post('/login', async (req, res) => {
   const { email, password } = req.body;
 
@@ -36,32 +35,27 @@ router.post('/login', async (req, res) => {
   }
 });
 
-// Google Login route
 router.post('/google-login', async (req, res) => {
-  const { token } = req.body; // The Google OAuth token sent by the client
+  const { token } = req.body;
 
   if (!token) {
     return res.status(400).json({ message: 'Please provide a Google token' });
   }
 
   try {
-    // Verify the Google token with Google's API
     const googleResponse = await axios.get(
       `https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=${token}`
     );
 
     const { email } = googleResponse.data;
 
-    // Check if the user already exists in the database
     let user = await User.findOne({ email });
 
     if (!user) {
-      // If the user doesn't exist, create a new user
       user = new User({ email });
       await user.save();
     }
 
-    // Generate JWT token for the user
     const payload = { userId: user._id };
     const jwtToken = jwt.sign(payload, process.env.JWT_SECRET_KEY, { expiresIn: '1h' });
 
